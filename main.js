934524834ae18e5fb0f2c8778f8148b7be6da272
knottem <walle.o.w@gmail.com>
2023-03-24 14:25:42 +0100

    added some code and removed modal

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-let shop = [];
+/*let shop = JSON.parse(localStorage.getItem('shop'));
+
+if(shop === null){
+    shop = []
+} else {
+    if (document.querySelector('.shopping')) {
+        shop.forEach(renderInDropdown)
+    }
+}
+*/
+let shop = []
 
 function addToCheckout(product) {
-    shop.push(product);
+   
+    if(shop.some(element => element.id === product.id)){
+        shop.find(element => element.id === product.id).quantity += 1;
+    } else {
+        shop.push(product);
+    }
+    
     localStorage.setItem('shop', JSON.stringify(shop));
     //renderInDropdown(product);
-    location.href = "checkout.html";
+    //location.href = "checkout.html";
 }
-// hej hej
 
+/*
 function renderProductCard(element) {
     let card = document.createElement('div');
     card.classList.add('card');
@@ -27,7 +43,43 @@ function renderProductCard(element) {
     });
 
 }
-/*
+*/
+
+function renderProductCard(element) {
+    let card = document.createElement('div');
+    card.classList.add('card', 'col-md-4', 'card-fixed-height');
+    card.innerHTML = `
+      <div class="row">
+        <div class="col-md-4">
+          <img src="${element.image}" class="img-fluid w-100" alt="card-horizontal-image">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${element.title}</h5>
+            <p class="card-text line-clamp">${element.description}</p>
+            <p class="card-text"><small class="text-muted">${element.rating.rate} rating (${element.rating.count} votes)</small></p>
+            <p class="card-text">${element.price} $</p>
+            <button class="btn btn-primary position-absolute bottom-0 start-50 translate-middle" data-bs-toggle="modal">Buy this item</button>
+          </div>
+         
+        </div>
+        
+      </div>
+      
+    `;
+
+    document.querySelector('.shopping').appendChild(card);
+
+    element.quantity = 1;
+
+    card.querySelector('.btn').addEventListener('click', () => {
+        addToCheckout(element);
+    });
+}
+
+
+
+
 function renderInDropdown(element) {
     let list = document.createElement('li');
     list.classList.add('dropdown-item');
@@ -44,9 +96,9 @@ function renderInDropdown(element) {
         //ta bort från arrayen och localstorage
     });
 }
-*/
 
-function renderCheckoutCard(element){
+
+function renderCheckoutCard(element) {
     let card = document.createElement('div');
     card.classList.add('card');
     card.innerHTML = `
@@ -60,11 +112,37 @@ function renderCheckoutCard(element){
     document.querySelector('.checkout').appendChild(card);
 }
 
+function renderCustomer(customer) {
+    let card = document.createElement('div');
+    card.innerHTML = `
+        <p>${customer.name}</p>
+        <p>${customer.email}</p>
+        <p>${customer.phone}</p>
+        <p>${customer.address}</p>
+        <p>${customer.zip}</p>
+        <p>${customer.city}</p>
+    `;
+
+    document.getElementById('customer').appendChild(card);
+}
+
+function renderConfirmationCard(element) {
+    let card = document.createElement('div');
+    card.innerHTML = `
+    <img src="${element.image}" alt="${element.title}" class="checkout-image">
+    <h4>${element.title}</h4>
+    <p>${element.price} $</p>
+    <p>${element.rating.rate} rating (${element.rating.count} votes)</p>
+    `;
+
+    document.getElementById('item').appendChild(card);
+}
+
 if (document.querySelector('.shopping')) {
     const TIMEOUT = 5000;
     const controller = new AbortController();
-    const timeoutId = setTimeout(() => {controller.abort();}, TIMEOUT);
-    
+    const timeoutId = setTimeout(() => { controller.abort(); }, TIMEOUT);
+
 
     //https://server.knotten.net/fakestore/
     //https://fakestoreapi.com/products
@@ -243,29 +321,31 @@ if (document.querySelector('.checkout')) {
                 'zip': zip.value,
                 'city': city.value
             }));
-            location.href="confirmation.html";
+            location.href = "confirmation.html";
         }
     }
 }
 
-// om vi har en modal på sidan
-if (document.querySelector('.modal')) {
-    console.log('modal found');
-    let modal = document.getElementById("myModal");
-    let span = document.getElementById("modalClose");
-    let button = document.getElementById("modalButton");
+if (document.querySelector('.confirmation')) {
 
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
+    const customer = JSON.parse(sessionStorage.getItem('customer'));
+    const shop = JSON.parse(localStorage.getItem('shop'))
+   
 
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+    /*
+    shop.forEach(product => {
+        if (product.id in productsCheck) {
+          productsCheck[product.id].quantity += product.quantity
+        } else {
+          productsCheck[product.id] = product
         }
-    }
+      })
+    renderCustomer(customer);
+    shop.forEach(renderConfirmationCard)
+    */
 
-    button.onclick = function () {
-        modal.style.display = "none";
-    }
+
+
+
+
 }
