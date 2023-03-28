d4ec54f2451c3d13b3b30624ab6ee0656f58bd03
knottem <59744487+knottem@users.noreply.github.com>
2023-03-28 20:29:48 +0200

    Update shopping.js, removed some code

--- js/shopping.js
+++ js/shopping.js
@@ -3,45 +3,41 @@ import { products } from "./products.js";
 
 let shop = []
 
-const TIMEOUT = 5000;
+const timeout = 5000;
 const controller = new AbortController();
-const timeoutId = setTimeout(() => { controller.abort(); }, TIMEOUT);
+const timeoutId = setTimeout(() => { controller.abort(); }, timeout);
 
 //https://server.knotten.net/fakestore/
 //https://fakestoreapi.com/products
 fetch('https://server.knotten.net/fakestore/', { signal: controller.signal })
-    .then(res => res.json()
-        .then(data => {
-            console.log("fetch success from api");
-            data.forEach(renderProductCard);
-            clearTimeout(timeoutId);
-        })
-        .catch(error => {
-            console.log(`Failed to parse response as JSON: ${error}`);
-            products.forEach(renderProductCard);
-        })
-    )
+  .then(res => res.json()
+    .then(data => {
+      console.log("fetch success from api");
+      data.forEach(renderProductCard);
+      clearTimeout(timeoutId);
+    })
     .catch(error => {
-        console.log(`Failed to fetch api after ${TIMEOUT / 1000} seconds, ` + error);
-        products.forEach(renderProductCard);
-    });
-
-
-
+      console.log(`Failed to parse response as JSON: ${error}`);
+      products.forEach(renderProductCard);
+    }))
+  .catch(error => {
+    console.log(`Failed to fetch api after ${timeout / 1000} seconds, ` + error);
+    products.forEach(renderProductCard);
+  });
 
 //add to checkout function
-function addToCheckout(product) {
-    let item = [product.id, product.quantity]
-    shop.push(item);
-    localStorage.setItem('shop', JSON.stringify(shop));
-    location.href = "checkout.html";
+function addToCheckout(id, quantity) {
+  let item = [id, quantity]
+  shop.push(item);
+  localStorage.setItem('shop', JSON.stringify(shop));
+  location.href = "checkout.html";
 }
 
 
 function renderProductCard(element) {
-    const card = document.createElement('div');
-    card.classList.add('col-sm-11', 'col-md-6', 'col-lg-3', 'col-xl-3', 'col-xxl-2', 'mb-4', 'card-container');
-    card.innerHTML = `
+  const card = document.createElement('div');
+  card.classList.add('col-sm-11', 'col-md-6', 'col-lg-3', 'col-xl-3', 'col-xxl-2', 'mb-4', 'card-container');
+  card.innerHTML = `
           <div class="card card-hover" style="border-radius: 15px;">
             <div class="text-center" style="height: 200px">
               <img src="${element.image}" style="border-top-left-radius: 15px; border-top-right-radius: 15px; max-height: 200px;" class="img-fluid" alt="${element.title}"/>
@@ -74,34 +70,31 @@ function renderProductCard(element) {
           <hr class="my-0" />
             <div class="card-body">
               <div class="d-flex justify-content-center align-items-center pb-2 mb-1">
-               
                 <button class="btn btn-primary">Buy now</button>
               </div>
             </div>
           </div>
     `;
-    document.querySelector('.row').appendChild(card);
+  document.querySelector('.row').appendChild(card);
 
-    element.quantity = 1;
+  card.querySelector('.btn').addEventListener('click', () => {
+    addToCheckout(element.id, 1);
+  });
 
-    card.querySelector('.btn').addEventListener('click', () => {
-        addToCheckout(element);
-    });
-
-    const descriptionContainerElement = card.querySelector('.description-container');
+  const descriptionContainerElement = card.querySelector('.description-container');
 
 
-    //adds the class 'has-more-text' to the description container if the description is longer than the container and shows a gradient at the bottom when it's not expanded
-    if (descriptionContainerElement.scrollHeight > descriptionContainerElement.clientHeight) {
-        descriptionContainerElement.classList.add('has-more-text');
-    }
+  //adds the class 'has-more-text' to the description container if the description is longer than the container and shows a gradient at the bottom when it's not expanded
+  if (descriptionContainerElement.scrollHeight > descriptionContainerElement.clientHeight) {
+    descriptionContainerElement.classList.add('has-more-text');
+  }
 
-    //made the description expand on hover and collapse on mouseleave of the entire card
-    descriptionContainerElement.addEventListener('mouseenter', () => {
-        descriptionContainerElement.classList.add('expanded');
-    });
+  //made the description expand on hover and collapse on mouseleave of the entire card
+  descriptionContainerElement.addEventListener('mouseenter', () => {
+    descriptionContainerElement.classList.add('expanded');
+  });
 
-    card.addEventListener('mouseleave', () => {
-        descriptionContainerElement.classList.remove('expanded');
-    });
+  card.addEventListener('mouseleave', () => {
+    descriptionContainerElement.classList.remove('expanded');
+  });
 }
