f97d2fe901799703a0aa0bcefc6e5e46df0d4054
knottem <walle.o.w@gmail.com>
2023-03-28 13:06:45 +0200

    split the js from one main

---
+++ js/confirmation.js
@@ -0,0 +1,58 @@
+const customer = JSON.parse(sessionStorage.getItem('customer'));
+const shop = JSON.parse(localStorage.getItem('shop'))
+shop.forEach(getProducts);
+
+
+function getProducts(element){
+    //"https://server.knotten.net/fakestore/" + id
+    fetch('https://fakestoreapi.com/products/' + element[0])
+    .then(res=>res.json())
+        .then(data=>renderConfirmationCard(data, element[1]))
+        .then(renderCustomer(customer))
+}
+
+function renderConfirmationCard(element, quantity) {
+    const confirmationCard = document.createElement('div');
+    confirmationCard.classList.add('col-sm-11', 'col-md-11', 'col-lg-7', 'mb-4');
+    confirmationCard.innerHTML = `
+    <div class="card" style="border-radius: 15px;">
+        <div class="row g-0">
+            <div class="col-md-2">
+                <img src="${element.image}" style="border-top-left-radius: 15px; border-bottom-left-radius: 15px; max-height: 150px; object-fit: cover;" class="img-fluid" alt="${element.title}" />
+            </div>
+            <div class="col-md-10">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <p class="title">${element.title}</p>
+                            <p class="small text-muted">${element.category}</p>
+                        </div>
+                        <div>
+                            <p>$${element.price}</p>
+                            <p>Quantity: ${quantity}</p>
+                            <p class="small text-muted">${element.rating.rate} rating (${element.rating.count} votes)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
+    document.getElementById('item').appendChild(confirmationCard);
+}
+
+
+
+function renderCustomer(customer) {
+    let card = document.createElement('div');
+    card.innerHTML = `
+        <p>${customer.name}</p>
+        <p>${customer.email}</p>
+        <p>${customer.phone}</p>
+        <p>${customer.address}</p>
+        <p>${customer.zip}</p>
+        <p>${customer.city}</p>
+    `;
+
+    document.getElementById('customer').appendChild(card);
+}
---
+++ js/shopping.js
@@ -0,0 +1,107 @@
+"use strict";
+import { products } from "./products.js";
+
+let shop = []
+
+const TIMEOUT = 5000;
+const controller = new AbortController();
+const timeoutId = setTimeout(() => { controller.abort(); }, TIMEOUT);
+
+//https://server.knotten.net/fakestore/
+//https://fakestoreapi.com/products
+fetch('https://server.knotten.net/fakestore/', { signal: controller.signal })
+    .then(res => res.json()
+        .then(data => {
+            console.log("fetch success from api");
+            data.forEach(renderProductCard);
+            clearTimeout(timeoutId);
+        })
+        .catch(error => {
+            console.log(`Failed to parse response as JSON: ${error}`);
+            products.forEach(renderProductCard);
+        })
+    )
+    .catch(error => {
+        console.log(`Failed to fetch api after ${TIMEOUT / 1000} seconds, ` + error);
+        products.forEach(renderProductCard);
+    });
+
+
+
+
+//add to checkout function
+function addToCheckout(product) {
+    let item = [product.id, product.quantity]
+    shop.push(item);
+    localStorage.setItem('shop', JSON.stringify(shop));
+    location.href = "checkout.html";
+}
+
+
+function renderProductCard(element) {
+    const card = document.createElement('div');
+    card.classList.add('col-sm-11', 'col-md-6', 'col-lg-3', 'col-xl-3', 'col-xxl-2', 'mb-4', 'card-container');
+    card.innerHTML = `
+          <div class="card card-hover" style="border-radius: 15px;">
+            <div class="text-center" style="height: 200px">
+              <img src="${element.image}" style="border-top-left-radius: 15px; border-top-right-radius: 15px; max-height: 200px;" class="img-fluid" alt="${element.title}"/>
+            </div>
+            <div class="card-body pb-0">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <p class="title">${element.title}</p>
+                  <p class="small text-muted">${element.category}</p>
+                </div>
+                <div>
+                <div class="d-flex flex-row justify-content-end mt-1 mb-4 text-danger">
+              </div>
+                </div>
+              </div>
+            </div>
+            <hr class="my-0" />
+            <div class="card-body pb-0">
+              <div class="d-flex justify-content-between">
+                <p>$${element.price}</p>
+                <p class="small text-muted">${element.rating.rate} rating (${element.rating.count} votes)</p>
+              </div>
+            </div>
+            <hr class="my-0" />
+            <div class="card-body pb-0">
+            <div class="description-container">
+              <p class="text-dark line-clamp">${element.description}</p>
+            </div>
+          </div>
+          <hr class="my-0" />
+            <div class="card-body">
+              <div class="d-flex justify-content-center align-items-center pb-2 mb-1">
+               
+                <button class="btn btn-primary">Buy now</button>
+              </div>
+            </div>
+          </div>
+    `;
+    document.querySelector('.row').appendChild(card);
+
+    element.quantity = 1;
+
+    card.querySelector('.btn').addEventListener('click', () => {
+        addToCheckout(element);
+    });
+
+    const descriptionContainerElement = card.querySelector('.description-container');
+
+
+    //adds the class 'has-more-text' to the description container if the description is longer than the container and shows a gradient at the bottom when it's not expanded
+    if (descriptionContainerElement.scrollHeight > descriptionContainerElement.clientHeight) {
+        descriptionContainerElement.classList.add('has-more-text');
+    }
+
+    //made the description expand on hover and collapse on mouseleave of the entire card
+    descriptionContainerElement.addEventListener('mouseenter', () => {
+        descriptionContainerElement.classList.add('expanded');
+    });
+
+    card.addEventListener('mouseleave', () => {
+        descriptionContainerElement.classList.remove('expanded');
+    });
+}
